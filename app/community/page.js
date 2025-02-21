e3b2ba2a3d7d44bfce49004172e499e628f164b5
anonyfinger <illa999511@gmail.com>
2025-02-21 19:58:39 +0900

    Menu active효과 추가

--- app/community/page.js
+++ app/community/page.js
@@ -4,7 +4,7 @@ import ConvenienceFoodCommunity from "@/components/ConvenienceFoodCommunity";
 
 export default function CommunityPage() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="community_container">
       <ConvenienceFoodCommunity />
     </div>
   );
--- components/Menu.js
+++ components/Menu.js
@@ -1,15 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+  const pathname = usePathname();
+
   return (
     <nav className="menu_container">
-      <Link href="/game" className="menu_link">
+      <Link
+        href="/game"
+        className={`menu_link ${
+          pathname === "/game" ? "menu_link_active" : ""
+        }`}
+      >
         심리 테스트
       </Link>
-      <Link href="/roulette" className="menu_link">
+      <Link
+        href="/roulette"
+        className={`menu_link ${
+          pathname === "/roulette" ? "menu_link_active" : ""
+        }`}
+      >
         룰렛
       </Link>
-      <Link href="/community" className="menu-item">
+      <Link
+        href="/community"
+        className={`menu_link ${
+          pathname === "/community" ? "menu_link_active" : ""
+        }`}
+      >
         커뮤니티
       </Link>
     </nav>
