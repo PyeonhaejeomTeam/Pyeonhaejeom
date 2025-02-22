6630d08e28fb2d0bf908d73b254d1538ab4d5358
anonyfinger <illa999511@gmail.com>
2025-02-22 10:40:17 +0900

    메타태그 수정

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,8 @@ export const metadata = {
     "편의점 음식 궁합",
     "편의점 음식 조합",
     "편의점 심리테스트",
+    "mbti 테스트",
+    "성향 테스트",
   ].join(", "),
 };
 
@@ -32,25 +34,19 @@ export default function RootLayout({ children }) {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta name="author" content="편해점" />
-        <meta name="creator" content="편해점" />
-        <meta name="publisher" content="편해점" />
         <meta name="category" content="entertainment" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="theme-color" content="#7c3aed" />
 
         {/* SEO 메타 태그 */}
         <meta name="robots" content="index, follow" />
-        <meta
-          name="keywords"
-          content="편의점,편의점 음식,음식 성향 테스트,편의점 음식 추천,편의점 꿀조합,편의점 메뉴 추천,편의점 음식 테스트,편의점 음식 궁합,편의점 음식 조합,편의점 심리테스트,mbti 테스트,성향 테스트"
-        />
         <meta name="application-name" content="편해점" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="편해점" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
-        {/* Open Graph 메타 태그 보완 */}
+        {/* Open Graph & Twitter Card 메타 태그 */}
         <meta
           property="og:title"
           content="편해점 - 당신의 편의점 성향 테스트"
@@ -69,23 +65,7 @@ export default function RootLayout({ children }) {
         <meta property="og:image:height" content="630" />
         <meta property="og:locale" content="ko_KR" />
         <meta property="og:type" content="website" />
-
-        {/* Twitter Card 메타 태그 보완 */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@편해점" />
-        <meta
-          name="twitter:title"
-          content="편해점 - 당신의 편의점 성향 테스트"
-        />
-        <meta
-          name="twitter:description"
-          content="당신의 편의점 음식 취향으로 알아보는 성향 테스트!"
-        />
-        <meta
-          name="twitter:image"
-          content="https://pyeonhaejeom.netlify.app/og-image.png"
-        />
-        <meta name="twitter:image:alt" content="편해점 테스트 미리보기" />
 
         {/* 검색엔진 최적화 */}
         <meta
@@ -103,9 +83,6 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
         />
-
-        {/* Favicon */}
-        <link rel="icon" href="/" />
         <link rel="apple-touch-icon" sizes="180x180" href="/creat8-2c.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
